f86c4a86baca63b633be7561da1a44e3671a8cd0
Mathilda <mathilda.magnusson@gmail.com>
2023-04-03 11:53:04 +0200

    hamstergram json

--- exercises/instagram/script.js
+++ exercises/instagram/script.js
@@ -1,6 +1,6 @@
 const contentElement = document.getElementById("content");
 const loginElement = document.getElementById("login");
-let currentUser = "Mathilda";
+let currentUser;
 
 let posts = [];
 for (let i = 1; i <= 32; i++) {
@@ -13,8 +13,8 @@ for (let i = 1; i <= 32; i++) {
 }
 
 function displayPosts() {
-    contentElement.innerHTML = "";
-    
+  contentElement.innerHTML = "";
+
   for (let post of posts) {
     const postElement = createPostElement(post);
     contentElement.appendChild(postElement);
@@ -39,11 +39,30 @@ function createPostElement(post) {
   articleElement.appendChild(infoElement);
 
   const likeButtonElement = document.createElement("button");
-  likeButtonElement.innerText = "🤍";
+  if (post.likes.indexOf(currentUser) === -1) {
+    likeButtonElement.innerText = "🤍";
+  } else {
+    likeButtonElement.innerText = "❤️";
+  }
+
+  if (currentUser) {
+    likeButtonElement.disabled = false;
+  } else {
+    likeButtonElement.disabled = true;
+  }
+
   likeButtonElement.addEventListener("click", () => {
-    post.likes.push(currentUser);
-    displayPosts();
+    const userIndex = post.likes.indexOf(currentUser);
+    if (userIndex === -1) {
+      post.likes.push(currentUser);
+    } else {
+      post.likes.splice(userIndex, 1);
+    }
+
+    const updatedPost = createPostElement(post);
+    articleElement.replaceWith(updatedPost);
   });
+
   infoElement.appendChild(likeButtonElement);
 
   const likeTextElement = document.createElement("span");
@@ -53,4 +72,65 @@ function createPostElement(post) {
   return articleElement;
 }
 
-displayPosts();
+function checkLogin() {
+  loginElement.innerHTML = "";
+
+  if (currentUser) {
+    loginElement.innerText = "Hi, " + currentUser + "!  ";
+    const logoutButton = document.createElement("button");
+    logoutButton.innerText = "Log out";
+    loginElement.appendChild(logoutButton);
+    logoutButton.addEventListener("click", () => {
+      currentUser = undefined;
+      checkLogin();
+      displayPosts();
+    });
+  } else {
+    const inputElement = document.createElement("input");
+    inputElement.placeholder = "Username";
+    loginElement.appendChild(inputElement);
+
+    const loginButton = document.createElement("button");
+    loginButton.innerText = "Login";
+    loginElement.appendChild(loginButton);
+    loginButton.addEventListener("click", () => {
+      if (inputElement.value.length > 0) {
+        currentUser = inputElement.value;
+        checkLogin();
+        displayPosts();
+      }
+    });
+  }
+}
+
+function loadJSON() {
+  const url = "images.JSON";
+  const method = "GET";
+
+  const request = new XMLHttpRequest();
+  request.responseType = "json";
+  request.open(method, url);
+  request.addEventListener("load", (event) => {
+    const request = event.target;
+    const response = request.response;
+    posts = response;
+    displayPosts();
+  });
+  request.send();
+}
+
+function fetchJSON() {
+  const url = "images.JSON";
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      posts = response;
+      displayPosts();
+    });
+}
+
+fetchJSON();
+//loadJSON();
+
+checkLogin();
+//displayPosts();
