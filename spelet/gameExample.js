b77b1a3dc4de4da58a1a2d2052e8ded6520f5ab0
Mathilda <mathilda.magnusson@gmail.com>
2023-04-18 15:22:46 +0200

    poop

--- spelet/gameExample.js
+++ spelet/gameExample.js
@@ -3,20 +3,22 @@ let bildArray = [];
 let counter = 0;
 
 function preload() {
-  img1 = loadImage("Artboard2.png");
-  img2 = loadImage("Artboard3.png");
-  img3 = loadImage("Artboard4.png");
-  img4 = loadImage("Artboard5.png");
+  img1 = loadImage("Artboardpd.png");
+  img2 = loadImage("Artboardpd.png");
+  img3 = loadImage("Artboardnn.png");
+  img4 = loadImage("Artboardnn.png");
   bildArray.push(img1, img2, img3, img4);
 }
 
 function setup() {
+  pixelDensity(1);
   createCanvas(1000, 1000);
   frameRate(10);
+
   // Resize the images
-  for (let i = 0; i < bildArray.length; i++) {
-    bildArray[i].resize(bildArray[i].width / 6, bildArray[i].height / 6);
-  }
+  /*for (let i = 0; i < bildArray.length; i++) {
+    bildArray[i].resize(bildArray[i].width / 2, bildArray[i].height / 2);
+  }*/
 }
 
 let bildX = 0;
@@ -26,5 +28,5 @@ function draw() {
   background(50, 50, 50);
   image(bildArray[counter], bildX, bildY);
   counter = (counter + 1) % bildArray.length;
-  bildX = bildX + 4;
+  //bildX = bildX + 4;
 }
